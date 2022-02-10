5c0ff320f5b29854dfd4c2d70d09403c91b43aee
anthonyde98 <anthonyde98@gmail.com>
2022-02-10 11:13:29 -0400

    Logica y interface finalizada, por el momento.

--- APIBooks/Services/Books/Book.cs
+++ APIBooks/Services/Books/Book.cs
@@ -1,11 +1,92 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using APIBooks.Models.Data;
 
 namespace APIBooks.Services.Books
 {
     public class Book: IBook
     {
+        public readonly APIBooksContext DbContext;
+
+        public Book(APIBooksContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public async Task<List<Models.Data.Libro>> Buscar()
+        {
+            var books = await DbContext.Libros.ToListAsync();
+
+            return books;
+        }
+
+        public async Task<bool> Existe(int id)
+        {
+            var respuesta = await DbContext.Libros.AnyAsync(l => l.LibroId == id);
+
+            return respuesta;
+        }
+
+        public async Task<Models.DTOs.Book.Book> Buscar(int id)
+        {
+            var Book = from book in DbContext.Libros
+                       where book.LibroId == id
+                       select new Models.DTOs.Book.Book()
+                       {
+                           Titulo = book.Titulo,
+                           Editorial = book.Editorial,
+                           Autor = book.Autor,
+                           FechaPublicacion = book.FechaPublicacion,
+                           Precio = book.Precio
+                       };
+
+            return await Book.SingleOrDefaultAsync();
+        }
+
+        public async Task<Models.DTOs.Book.Book> Crear(Models.DTOs.Book.Book book)
+        {
+            DbContext.Libros.Add(new Models.Data.Libro()
+            {
+                Titulo = book.Titulo,
+                Editorial = book.Editorial,
+                Autor = book.Autor,
+                FechaPublicacion = book.FechaPublicacion,
+                Precio = book.Precio
+            });
+
+            await DbContext.SaveChangesAsync();
+
+            return book;
+        }
+
+        public async Task<Models.DTOs.Book.Book> Editar(int id, Models.DTOs.Book.Book book)
+        {
+            var recentBook = await DbContext.Libros.FindAsync(id);
+
+            recentBook.LibroId = id;
+            recentBook.Titulo = book.Titulo == null ? recentBook.Titulo : book.Titulo;
+            recentBook.Autor = book.Autor == null ? recentBook.Autor : book.Autor;
+            recentBook.Editorial = book.Editorial == null ? recentBook.Editorial : book.Editorial;
+            recentBook.FechaPublicacion = book.FechaPublicacion;
+
+            DbContext.Update(recentBook);
+
+            await DbContext.SaveChangesAsync();
+
+            return await Buscar(id);
+        }
+
+        public async Task<string> Eliminar(int id)
+        {
+            var book = await DbContext.Libros.FindAsync(id);
+
+            DbContext.Remove(book);
+            await DbContext.SaveChangesAsync();
+
+            return "Deleted";
+        }
     }
 }
--- APIBooks/Services/Books/IBook.cs
+++ APIBooks/Services/Books/IBook.cs
@@ -7,5 +7,11 @@ namespace APIBooks.Services.Books
 {
     public interface IBook
     {
+        Task<List<Models.Data.Libro>> Buscar();
+        Task<bool> Existe(int id);
+        Task<Models.DTOs.Book.Book> Buscar(int id);
+        Task<Models.DTOs.Book.Book> Crear(Models.DTOs.Book.Book book);
+        Task<Models.DTOs.Book.Book> Editar(int id, Models.DTOs.Book.Book book);
+        Task<string> Eliminar(int id);
     }
 }
